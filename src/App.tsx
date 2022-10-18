5f1342ef85e588c8b129e5ac75ff657ff4b98597
cawauchi <tatsuya.kawauchi@photocreate.co.jp>
2022-10-18 19:39:14 +0900

    add: template

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "react-beautiful-dnd";
+
+import "./App.css";
 
 function App() {
+  const [items, setItems] = useState([
+    { id: 1, text: "item0" },
+    { id: 2, text: "item1" },
+    { id: 3, text: "item2" },
+  ]);
+
+  const onDragEnd = (result: DropResult) => {
+    if (!result.destination) return console.log("destinationがありません！");
+    const newItems = Array.from(items);
+    const [reorderedItem] = newItems.splice(result.source.index, 1);
+    newItems.splice(result.destination.index, 0, reorderedItem);
+
+    setItems(newItems);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="dragDropArea">
+      <DragDropContext onDragEnd={onDragEnd}>
+        <Droppable droppableId="droppable">
+          {(provided, snapShot) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {/* innerRefはドラッグしている以外の要素の制御をしている */}
+              {items.map((item, index) => (
+                <Draggable
+                  key={item.id}
+                  draggableId={String(item.text)}
+                  index={index}
+                >
+                  {(provided) => (
+                    <div
+                      className="item"
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      {item.text}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {/* draggableの領域を広げる */}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 }
